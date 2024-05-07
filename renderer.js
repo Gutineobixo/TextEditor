f0624d48f12225d211d2c1eef140a7291266ab34
Rodz-Vskmann <augustozuanazzi03@gmail.com>
2024-05-07 07:05:24 -0300

    Update v.1.2

--- renderer.js
+++ renderer.js
@@ -39,16 +39,27 @@ function alternarMarcaTexto() {
 
     if (selectedText.length === 0) return; // Se não há texto selecionado, retorna
 
-    const documentFragment = range.extractContents(); // Extrair o conteúdo da seleção
-    const span = document.createElement('span'); // Criar um novo elemento span
-    span.style.backgroundColor = highlightColor; // Definir a cor de fundo do span
-    span.appendChild(documentFragment); // Adicionar o conteúdo extraído ao span
+    // Verifica se o elemento selecionado já é um span com a cor de destaque
+    const parentNode = range.startContainer.parentNode;
+    if (parentNode.tagName === "SPAN" && parentNode.style.backgroundColor === highlightColor) {
+        // Desmarca o texto removendo o span e restaurando o texto
+        const textNode = document.createTextNode(parentNode.innerText);
+        parentNode.parentNode.replaceChild(textNode, parentNode);
+        const newRange = document.createRange();
+        newRange.selectNodeContents(textNode);
+        selection.removeAllRanges();
+        selection.addRange(newRange);
+    } else {
+        const documentFragment = range.extractContents(); // Extrair o conteúdo da seleção
+        const span = document.createElement('span'); // Criar um novo elemento span
+        span.style.backgroundColor = highlightColor; // Definir a cor de fundo do span
+        span.appendChild(documentFragment); // Adicionar o conteúdo extraído ao span
+        range.insertNode(span); // Inserir o span no lugar original
 
-    range.insertNode(span); // Inserir o span no lugar original
-
-    // Limpar a seleção para evitar múltiplas marcações ao clicar repetidamente
-    selection.removeAllRanges();
-    const newRange = document.createRange(); // Criar um novo range
-    newRange.selectNodeContents(span); // Selecionar o conteúdo do novo span
-    selection.addRange(newRange); // Adicionar o novo range à seleção
+        // Limpar a seleção para evitar múltiplas marcações ao clicar repetidamente
+        selection.removeAllRanges();
+        const newRange = document.createRange(); // Criar um novo range
+        newRange.selectNodeContents(span); // Selecionar o conteúdo do novo span
+        selection.addRange(newRange); // Adicionar o novo range à seleção
+    }
 }
